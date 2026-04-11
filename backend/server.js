dd75ed3238338680f389ce6c0e04415dea5c85cd
Abdelrahman Ashraf <abdelrahmanashraf6000@gmail.com>
2026-04-11 18:44:37 +0200

    refactor: made routes compined in routes/router.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import mediaRoutes from "./routes/media.routes.js";
+import router from "./routes/router.js";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/media", mediaRoutes);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
