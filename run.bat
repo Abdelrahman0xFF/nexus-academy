4801560d2bc6bda649427d5d4e8c65b098fc2934
Abdelrahman Ashraf <abdelrahmanashraf6000@gmail.com>
2026-05-06 21:11:32 +0300

    fix: update stripe webhook URL in run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-start cmd /k "stripe listen --forward-to localhost:4000/webhook"
+start cmd /k "stripe listen --forward-to localhost:4000/api/payments/webhook"
 start cmd /k "cd .\backend && npm install && npm start"
 start cmd /k "cd .\frontend && npm install && npm run dev"
 
